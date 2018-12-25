b30eeb772cfd20c41b29bc676f2fd666be1822d9
jpontesmartins <jpontesmartins@gmail.com>
2018-12-25 09:15:55 -0200

    primeiro commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        exclude: '/node_modules',
+        use: {
+          loader: "html-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+
+}
