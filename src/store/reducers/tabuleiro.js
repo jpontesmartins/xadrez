7ecd0d2b820a8e7b7ae558e0d2491a1cb1734d40
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-04-09 14:52:09 -0300

    refactor

--- src/store/reducers/tabuleiro.js
+++ src/store/reducers/tabuleiro.js
@@ -31,49 +31,49 @@ export default (state = INITIAL_STATE, action) => {
     case 'MOVER_PECA':
       const { coluna, linha, peca, cor, casaOrigem } = action.payload;
       const destino = { peca, linha, cor, coluna };
-      const colunaOrigem = casaOrigem.split("")[0];
-      const colunaDestino = coluna;
       console.log(`Casa origem: ${casaOrigem}`);
       console.log(`Casa destino: ${coluna}${linha}`);
 
-      if (colunaOrigem === colunaDestino) {
-        return {
-          ...state,
-          colunaA: move(A, casaOrigem, state, destino),
-          colunaB: move(B, casaOrigem, state, destino),
-          colunaC: move(C, casaOrigem, state, destino),
-          colunaD: move(D, casaOrigem, state, destino),
-          colunaE: move(E, casaOrigem, state, destino),
-          colunaF: move(F, casaOrigem, state, destino),
-          colunaG: move(G, casaOrigem, state, destino),
-          colunaH: move(H, casaOrigem, state, destino),
-        }
-      } else {
-
-        return {
-          ...state,
-          colunaA: movimentacao(A, casaOrigem, state, destino),
-          colunaB: movimentacao(B, casaOrigem, state, destino),
-          colunaC: movimentacao(C, casaOrigem, state, destino),
-          colunaD: movimentacao(D, casaOrigem, state, destino),
-          colunaE: movimentacao(E, casaOrigem, state, destino),
-          colunaF: movimentacao(F, casaOrigem, state, destino),
-          colunaG: movimentacao(G, casaOrigem, state, destino),
-          colunaH: movimentacao(H, casaOrigem, state, destino),
-
-        }
-      }
+      return moveAll(casaOrigem, state, destino);
     default:
       return state
   }
 }
 
-const esvaziaCasaDaPecaMovimentada = (casaOrigem, allColumns) => {
-  return movePieces.esvaziaCasaDaPecaMovimentada(casaOrigem, allColumns);
-}
+const moveAll = (casaOrigem, state, destino) => {
+  const colunaOrigem = casaOrigem.split("")[0];
+  const colunaDestino = destino.coluna;
+  const { coluna, linha } = destino.coluna;
 
-const movePieceToAnotherColumn = (casaOrigem, coluna, colunaCompleta, peca, linha, cor) => {
-  return movePieces.movePieceToAnotherColumn(casaOrigem, coluna, colunaCompleta, peca, linha, cor);
+  console.log(`Casa origem: ${casaOrigem}`);
+  console.log(`Casa destino: ${coluna}${linha}`);
+  
+  if (colunaOrigem === colunaDestino) {
+    return {
+      ...state,
+      colunaA: move(A, casaOrigem, state, destino),
+      colunaB: move(B, casaOrigem, state, destino),
+      colunaC: move(C, casaOrigem, state, destino),
+      colunaD: move(D, casaOrigem, state, destino),
+      colunaE: move(E, casaOrigem, state, destino),
+      colunaF: move(F, casaOrigem, state, destino),
+      colunaG: move(G, casaOrigem, state, destino),
+      colunaH: move(H, casaOrigem, state, destino),
+    }
+  } else {
+    return {
+      ...state,
+      colunaA: movimentacao(A, casaOrigem, state, destino),
+      colunaB: movimentacao(B, casaOrigem, state, destino),
+      colunaC: movimentacao(C, casaOrigem, state, destino),
+      colunaD: movimentacao(D, casaOrigem, state, destino),
+      colunaE: movimentacao(E, casaOrigem, state, destino),
+      colunaF: movimentacao(F, casaOrigem, state, destino),
+      colunaG: movimentacao(G, casaOrigem, state, destino),
+      colunaH: movimentacao(H, casaOrigem, state, destino),
+    }
+  }
+
 }
 
 function move(colunaAtual, casaOrigem, state, destino) {
@@ -96,11 +96,11 @@ function movimentacao(colunaAtual, casaOrigem, state, destino) {
 
   const colunaOrigem = casaOrigem.split("")[0];
   if (colunaOrigem == colunaAtual) {
-    return esvaziaCasaDaPecaMovimentada(casaOrigem, allColumns);
+    return movePieces.esvaziaCasaDaPecaMovimentada(casaOrigem, allColumns);
   }
 
   if (coluna == colunaAtual) {
-    return movePieceToAnotherColumn(casaOrigem, coluna, allColumns.get(coluna), peca, linha, cor);
+    return movePieces.movePieceToAnotherColumn(casaOrigem, coluna, allColumns.get(coluna), peca, linha, cor);
   } else {
     return allColumns.get(colunaAtual);
   }
